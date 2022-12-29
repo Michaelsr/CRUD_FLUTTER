a50f448de48a1189c9ed388c7d430974cafb19db
Michaelsr <michaelsonccoramos@gmail.com>
2022-12-29 14:18:00 -0500

    CRUD

--- lib/src/db/sql_helper.dart
+++ lib/src/db/sql_helper.dart
@@ -35,4 +35,57 @@ class SQLHelper {
       },
     );
   }
+
+  // Create new item (journal)
+  static Future<int> createItem(
+      String nombre, String? tipo, String? detalle, String? valor) async {
+    final db = await SQLHelper.db();
+
+    final data = {'nombre': nombre, 'detalle': detalle, 'valor': valor};
+    final id = await db.insert('boleta', data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    return id;
+  }
+
+  // Read all items (journals)
+  static Future<List<Map<String, dynamic>>> getItems() async {
+    final db = await SQLHelper.db();
+    return db.query('boleta', orderBy: "id_boleta");
+  }
+
+  // Read a single item by id
+  // The app doesn't use this method but I put here in case you want to see it
+  static Future<List<Map<String, dynamic>>> getItem(int id) async {
+    final db = await SQLHelper.db();
+    return db.query('boleta',
+        where: "id_boleta = ?", whereArgs: [id], limit: 1);
+  }
+
+  // Update an item by id
+  static Future<int> updateItem(int id, String nombre, String? tipo,
+      String? detalle, String? valor) async {
+    final db = await SQLHelper.db();
+
+    final data = {
+      'nombre': nombre,
+      'tipo': tipo,
+      'detalle': detalle,
+      'valor': valor,
+      'createdAt': DateTime.now().toString()
+    };
+
+    final result = await db
+        .update('boleta', data, where: "id_boleta = ?", whereArgs: [id]);
+    return result;
+  }
+
+  // Delete
+  static Future<void> deleteItem(int id) async {
+    final db = await SQLHelper.db();
+    try {
+      await db.delete("boleta", where: "id_boleta = ?", whereArgs: [id]);
+    } catch (err) {
+      debugPrint("Something went wrong when deleting an item: $err");
+    }
+  }
 }
